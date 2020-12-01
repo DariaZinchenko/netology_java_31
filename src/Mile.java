cb1f5b58e22e3aad8c2d9104d12a220b5fd47ecc
Daria Zinchenko <dariadaria0593@gmail.com>
2020-12-01 22:36:07 +0300

    add new Class BonusMilesService

--- src/Mile.java
+++ src/Mile.java
@@ -1,13 +1,8 @@
 public class Mile {
     public static void main (String[] args) {
-        int amountInKopecks = 253043;
-        int coefficient = 20*100;
-        int miles = calculateMiles(amountInKopecks, coefficient);
-        System.out.println("Стоимость билета = " + amountInKopecks);
-        System.out.println("Начисленные мили = " + miles);
-    }
-
-    public static int calculateMiles (int amountInKopecks, int coefficient) {
-        return amountInKopecks/coefficient;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000; //цена указывается в копейках
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
